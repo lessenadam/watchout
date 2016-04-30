4b488d73437cbde4a46e65ed2ee336454e09aac1
lessenadam <lessena@gmail.com>
2016-04-29 21:16:08 -0700

    Add hero movement and collision detection
    - coordinates are not properly updating during hero movement

--- client/watchout.js
+++ client/watchout.js
@@ -1,27 +1,92 @@
 // start slingin' some d3 here.
-var data = [10, 20, 30, 40, 50, 60];
+var data = [{x: Math.random() * 800, y: Math.random() * 800, r: Math.random() * 80 + 10}];
+
+var heroData = [{x: Math.random() * 800, y: Math.random() * 800, r: 25}];
 
 var svg = d3.select('.board').append("svg").attr("width", 800).attr("height", 800);
 
+var drag = d3.behavior.drag()
+        .origin(function(d) { return {x:d.x, y:d.y}; })
+        .on('dragstart', function (d) {
+
+        })
+        .on('drag', function (d) {
+            var xCoord = d.x;
+            // console.log("dx was," + xCoord);
+            d.x += d3.event.dx;
+           
+
+            d.y += d3.event.dy;
+            d3.select(this)
+                .attr('transform', 'translate(' + d.x + ',' + d.y + ')');
+            // console.log("dx is now," + d.x);
+        })
+        .on('dragend', function (d) {
+
+        });
+
 var asteroids = svg
-.selectAll("circle")
-.data(data)
-.enter()
-.append("circle");
+  .selectAll("circle")
+  .data(data)
+  .enter()
+  .append("circle");
 
 asteroids
-.attr("r", d => d)
-.attr("cx", d => Math.random() * 800)
-.attr("cy", d => Math.random() * 800);
+  .attr("r", d => d.r)
+  .attr("cx", d => d.x)
+  .attr("cy", d => d.y)
+  .attr("class", "enemy");
 
 var player = svg
-.selectAll(".hero")
-.data([25])
-.enter()
-.append("circle");
+  .selectAll(".hero")
+  .data(heroData)
+  .enter()
+  .append("circle")
+  .call(drag);
 
 player
-.attr("r", d => d)
-.attr("cx", Math.random() * 800)
-.attr("cy", Math.random() * 800)
-.style('fill', "yellow");
+  .attr("r", d => d.r)
+  .attr("cx", d => d.x)
+  .attr("cy", d => d.y)
+  .style('fill', "yellow")
+  .attr('class', 'hero');
+
+
+// click dragging 
+
+
+
+
+// collision detection 
+var collisions = 0;
+
+function collision () {
+  d3.selectAll('.enemy')
+  .each(node => {
+    var heroR = +d3.select('.hero').attr('r');
+    var heroY = +d3.select('.hero').attr('cy');
+    var heroX = +d3.select('.hero').attr('cx');
+    var nodeR = node.r;
+    var nodeX = node.x;
+    var nodeY = node.y;
+    var distance = Math.pow(Math.pow(nodeX - heroX, 2) + Math.pow(nodeY - heroY, 2), .5);
+    if (distance < heroR + nodeR) {
+      collisions++;
+    }
+  });
+}
+
+
+setInterval(collision, 100);
+setInterval(function(){
+  if (collisions > 0) {
+    console.log("COLLISION");
+  }
+}, 1000);
+
+
+// asteroid movement 
+
+
+
+// scoring function 
